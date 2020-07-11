edabd17b61cc848ed442e9abf03cb4e0b1db12ab
Bhagya Sri Pottumuthu <bpottumu@oradev.oraclecorp.com>
2020-07-11 19:31:10 +0530

    DATA

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 
